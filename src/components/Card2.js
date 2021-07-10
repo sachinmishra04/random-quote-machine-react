3bdd278191694295119533bd89011e891d2a98ed
sachinmishra <sachinmishra0422@gmail.com>
2021-07-10 08:28:41 +0530

    change text and flex direction in mobile view

--- src/components/Card2.js
+++ src/components/Card2.js
@@ -2,67 +2,75 @@
  * Project:  random-quote-machine
  * File:      Card2.js
  * Author:   Amit Mishra (amit.mishra+CODA@gmail.com)
- * 
+ *
  * Description:
- * 
+ *
  * Revision History:
- *   2021-January-21: 
- * 
+ *   2021-January-21:
+ *
  * Copyright (c) 2021 Self
- * 
+ *
  * License:
- *    
+ *
  */
 
 import React from "react";
 import "./Card.css";
 import Twitter from "./Twitter";
-import logo from "./Twitter_Social_Icon_Circle_Color.png"
+import logo from "./Twitter_Social_Icon_Circle_Color.png";
 
-const Card2=(props)=>{
+const Card2 = (props) => {
+  function giveValidAuthor() {
+    return props.author === null ? "Unknown" : props.author;
+  }
 
-    function giveValidAuthor(){
-        return (props.author===null)?"Unknown":props.author;
-    }
-    
+  function onCardClick() {
+    props.quote();
+  }
 
-    function onCardClick(){
-       props.quote();
-       
-    }
+  return (
+    <div className="box">
+      <div id="quote-box" className="ui card-css cards">
+        <div className=" ui raised card">
+          <div className="content">
+            <p
+              id="text"
+              style={{ color: props.color }}
+              className={`${props.color} center aligned big description`}
+            >
+              "{props.text}"
+            </p>
+            <div
+              id="author"
+              style={{ color: props.color }}
+              className={`center aligned ${props.color} header`}
+            >{`-${giveValidAuthor()}`}</div>
+          </div>
 
-    return(
-        <div className="box" >
-            <div id="quote-box" className="ui card-css cards">
-                <div className=" ui raised card">
-                    <div className="content">
-                        
-                        <p id="text" style={{color:props.color}} className={`${props.color} center aligned big description`}>
-                           "{props.text}" 
-                        </p>
-                        <div id="author" style={{color:props.color}} className={`center aligned ${props.color} header`}>{`-${giveValidAuthor()}`}</div>
-                    </div>
-
-                    
-                        <div class="ui two buttons">
-                           <div> <a  href={`https://twitter.com/intent/tweet?text=${props.text}-${props.author}`} id="tweet-quote" class="ui basic green button">
-                           <img src={logo} style={{width:`48px`,height:`48px`}} />
-                            </a>
-                           
-                            </div>
-                            <button id="new-quote" onClick={onCardClick} 
-                            className={`${props.color} ui inverted button` }
-                            style={{backgroundColor:props.color}}> 
-                            Next Quote
-                            </button>
-                            
-                        </div>
-                    
-            
-                </div>
+          <div class="ui two buttons">
+            <div>
+              {" "}
+              <a
+                href={`https://twitter.com/intent/tweet?text=${props.text}-${props.author}`}
+                id="tweet-quote"
+                class="ui basic green button"
+              >
+                <img src={logo} />
+              </a>
             </div>
+            <button
+              id="new-quote"
+              onClick={onCardClick}
+              className={`${props.color} ui inverted button`}
+              style={{ backgroundColor: props.color }}
+            >
+              Next Quote
+            </button>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Card2;
